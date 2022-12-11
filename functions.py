b0b6bafcb5a8dbb949fb3345232d100f17d515dd
OstZ <zsm1597183514@outlook.com>
2022-12-11 22:20:43 +0800

    AMP

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def local_kemeny(pairs,init_rank):
     '''
     input (u,i,j) pairs and init_rank([3,1,0,9,6...])
@@ -28,20 +29,47 @@ def local_kemeny(pairs,init_rank):
 def AMP(user_pairs, ref_rank,phi):
     '''
     return rank sampled from specific phi
-    :param user_pairs:a nx2 np.array
+    :param user_pairs:pairs represent preferences of one user
     :param ref_rank:a list
     :param phi:a float parameter
-    :return:
+    :return: a rank
     '''
     rank = []
+
     #initial rank
     rank.append(ref_rank[0])
+
+    #turn pairs to a list
     u_pair = [tuple(pair) for pair in user_pairs]
+
     #every loop insert a item
     for i in range(1,len(ref_rank)):
+        L_i = []
+        H_i = []
+        sigma_i = ref_rank[i]
         #i' that less than i
         for j in range(i-1,-1,-1):
-            if
+            r_j = rank[j]
+            if (r_j, sigma_i) in u_pair:
+                L_i.append(j)
+            if (sigma_i,r_j) in u_pair:
+                H_i.append(j)
+        # print("L_i is:", L_i)
+        # print("H_i is:", H_i)
+        l_i = 0 if len(L_i)==0 else max(L_i)+1
+        h_i = i if len(H_i)==0 else min(H_i)
+        #print("l_i and h_i:",l_i,h_i)
+
+        positions = [idx for idx in range(l_i, h_i + 1)]
+
+        #print("candidate positions:", positions)
+        probs = [np.power(phi, i - j) for j in positions]
+        probs = np.divide(probs, sum(probs))
+        in_idx = np.random.choice(positions, p=probs)
+        rank.insert(in_idx, sigma_i)
+
+    return rank
+
 
 
 
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import numpy as np
 import random
 from utils import load_data
-from functions import local_kemeny
+from functions import local_kemeny, AMP
 
 if __name__ == '__main__':
     path = 'sushi3-2016'
     num_u, num_i, uids, iid1, iid2, init_rank = load_data(path)
     random.shuffle(init_rank)
-    print(init_rank)
+    print("initial rank is:", init_rank)
 
     pairs = np.array([[i, j] for u, i, j in zip(uids, iid1, iid2)])
     ref_rank = local_kemeny(pairs, init_rank)
-    print(ref_rank)
+    print("reference rank is:", ref_rank)
+
+    user_pair = pairs[46:60]
+    print(AMP(user_pair,ref_rank,0.5))
+
+
